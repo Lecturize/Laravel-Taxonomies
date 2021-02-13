84ff63b9110738b03470b30d0293563a663966cf
AlexanderPoellmann <alex@lecturize.com>
2021-02-13 20:32:17 +0100

    Add new method signatures

--- src/Facades/Taxonomy.php
+++ src/Facades/Taxonomy.php
@@ -1,13 +1,17 @@
 <?php namespace Lecturize\Taxonomies\Facades;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
+
 use Lecturize\Taxonomies\Models\Taxonomy as TaxonomyModel;
 
 /**
  * Class Taxonomy
  * @package Lecturize\Taxonomies\Facades
  *
- * @method static \Illuminate\Support\Collection createCategories(string|array $categories, string $taxonomy, TaxonomyModel $parent = null, int $sort = null)
+ * @method static Collection createCategories(string|array $categories, string $taxonomy, TaxonomyModel $parent = null, int $sort = null)
+ * @method static Collection getTree(string $taxonomy, string $taxable_class = '', boolean $cached = true)
+ * @method static Collection buildTree( Collection $taxonomies, string $taxable_class, boolean $is_child = false)
  */
 class Taxonomy extends Facade
 {
